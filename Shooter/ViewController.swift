fb80de9a45720368ddbbdd4969025b339e0264c4
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-27 15:17:00 +0900

    Initial Commit

---
+++ Shooter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shooter
+//
+//  Created by Julian Lechuga Lopez on 27/6/18.
+//  Copyright © 2018 Julian Lechuga Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
