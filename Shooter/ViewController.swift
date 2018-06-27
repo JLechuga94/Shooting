856fe262edc88cd3affcf4578bbd21ae274872c2
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-27 15:59:59 +0900

    Added bullet node to the camera position and orientation for adding the node in that position. Added tapGestureRecognizer for func trigger of bulletNode adding. Added func removeEveryotherBullet to delete any other node when shooting a new bullet

--- Shooter/ViewController.swift
+++ Shooter/ViewController.swift
@@ -13,13 +13,43 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
+    var power: Float = 50
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.sceneView.session.run(configuration)
         self.sceneView.autoenablesDefaultLighting = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func removeEveryOtherBullet(){
+        self.sceneView.scene.rootNode.enumerateChildNodes{(node, _) in
+            if node.name == "Bullet"{
+                node.removeFromParentNode()
+            }
+        }
+    }
+
+    @objc func handleTap(sender: UITapGestureRecognizer){
+        guard let pointOfView = self.sceneView.pointOfView else {return}
+        self.removeEveryOtherBullet()
+        let transform = pointOfView.transform
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        let position = location + orientation
+        let bullet = SCNNode(geometry: SCNSphere(radius: 0.1))
+        bullet.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        bullet.position = position
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: bullet))
+        bullet.physicsBody = body
+        bullet.name = "Bullet"
+//        body.restitution = 0.2
+        bullet.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
+        self.sceneView.scene.rootNode.addChildNode(bullet)
+        
+    }
+    
     
     @IBAction func addTargets(_ sender: Any) {
         self.addEgg(x:5, y: 0, z: -40)
@@ -36,4 +66,6 @@ class ViewController: UIViewController {
 
 
 }
-
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
