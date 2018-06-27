e936f141eb508db0214ccedb31dc5dc3cbf55ee9
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-27 15:33:52 +0900

    Added egg scene file for targets and button for adding 3 egg nodes to the scene

--- Shooter/ViewController.swift
+++ Shooter/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import ARKit
 class ViewController: UIViewController {
 
+   
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
@@ -18,6 +19,19 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration)
         self.sceneView.autoenablesDefaultLighting = true
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func addTargets(_ sender: Any) {
+        self.addEgg(x:5, y: 0, z: -40)
+        self.addEgg(x: 0, y: 0, z: -40)
+        self.addEgg(x: -5, y: 0, z: -40)
+    }
+    
+    func addEgg(x: Float, y: Float, z: Float){
+        let eggScene = SCNScene(named: "Media.scnassets/egg.scn")
+        let eggNode = (eggScene?.rootNode.childNode(withName: "egg", recursively: false))!
+        eggNode.position = SCNVector3(x,y,z)
+        self.sceneView.scene.rootNode.addChildNode(eggNode)
     }
 
 
